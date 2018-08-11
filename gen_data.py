68aef49068a2a360da2743087306e98461578782
vivekverma239 <vivekverma239@gmail.com>
2018-08-11 16:51:39 +0530

    Initial Commit

--- gen_data.py
+++ gen_data.py
@@ -116,44 +116,44 @@ def generate_training_data(vocab_ids, writer_lm_all, writer_seq_ae_all):
     input_seq = build_input_sequence(doc, vocab_ids)
     if len(input_seq) < 2:
       continue
-    rev_seq = data.build_reverse_sequence(input_seq)
+    # rev_seq = data.build_reverse_sequence(input_seq)
     lm_seq = data.build_lm_sequence(input_seq)
-    rev_lm_seq = data.build_lm_sequence(rev_seq)
-    seq_ae_seq = data.build_seq_ae_sequence(input_seq)
+    # rev_lm_seq = data.build_lm_sequence(rev_seq)
+    # seq_ae_seq = data.build_seq_ae_sequence(input_seq)
     if doc.label is not None:
       # Used for sentiment classification.
       label_seq = data.build_labeled_sequence(
           input_seq,
           doc.label,
           label_gain=(FLAGS.label_gain and not doc.is_validation))
-      bd_label_seq = data.build_labeled_sequence(
-          data.build_bidirectional_seq(input_seq, rev_seq),
-          doc.label,
-          label_gain=(FLAGS.label_gain and not doc.is_validation))
+    #   bd_label_seq = data.build_labeled_sequence(
+    #       data.build_bidirectional_seq(input_seq, rev_seq),
+    #       doc.label,
+    #       label_gain=(FLAGS.label_gain and not doc.is_validation))
       class_writer = writer_valid_class if doc.is_validation else writer_class
-      bd_class_writer = (writer_bd_valid_class
-                         if doc.is_validation else writer_bd_class)
+    #   bd_class_writer = (writer_bd_valid_class
+                        #  if doc.is_validation else writer_bd_class)
       class_writer.write(label_seq.seq.SerializeToString())
-      bd_class_writer.write(bd_label_seq.seq.SerializeToString())
+    #   bd_class_writer.write(bd_label_seq.seq.SerializeToString())
 
     # Write
     lm_seq_ser = lm_seq.seq.SerializeToString()
     seq_ae_seq_ser = seq_ae_seq.seq.SerializeToString()
     writer_lm_all.write(lm_seq_ser)
-    writer_seq_ae_all.write(seq_ae_seq_ser)
+    # writer_seq_ae_all.write(seq_ae_seq_ser)
     if not doc.is_validation:
       writer_lm.write(lm_seq_ser)
       writer_rev_lm.write(rev_lm_seq.seq.SerializeToString())
-      writer_seq_ae.write(seq_ae_seq_ser)
+    #   writer_seq_ae.write(seq_ae_seq_ser)
 
   # Close writers
   writer_lm.close()
-  writer_seq_ae.close()
+  # writer_seq_ae.close()
   writer_class.close()
   writer_valid_class.close()
-  writer_rev_lm.close()
-  writer_bd_class.close()
-  writer_bd_valid_class.close()
+  # writer_rev_lm.close()
+  # writer_bd_class.close()
+  # writer_bd_valid_class.close()
 
 
 def generate_test_data(vocab_ids, writer_lm_all, writer_seq_ae_all):
@@ -170,31 +170,31 @@ def generate_test_data(vocab_ids, writer_lm_all, writer_seq_ae_all):
     input_seq = build_input_sequence(doc, vocab_ids)
     if len(input_seq) < 2:
       continue
-    rev_seq = data.build_reverse_sequence(input_seq)
+    # rev_seq = data.build_reverse_sequence(input_seq)
     lm_seq = data.build_lm_sequence(input_seq)
-    rev_lm_seq = data.build_lm_sequence(rev_seq)
-    seq_ae_seq = data.build_seq_ae_sequence(input_seq)
+    # rev_lm_seq = data.build_lm_sequence(rev_seq)
+    # seq_ae_seq = data.build_seq_ae_sequence(input_seq)
     label_seq = data.build_labeled_sequence(input_seq, doc.label)
-    bd_label_seq = data.build_labeled_sequence(
-        data.build_bidirectional_seq(input_seq, rev_seq), doc.label)
+    # bd_label_seq = data.build_labeled_sequence(
+    #     data.build_bidirectional_seq(input_seq, rev_seq), doc.label)
 
     # Write
     writer_class.write(label_seq.seq.SerializeToString())
-    writer_bd_class.write(bd_label_seq.seq.SerializeToString())
+    # writer_bd_class.write(bd_label_seq.seq.SerializeToString())
     lm_seq_ser = lm_seq.seq.SerializeToString()
     seq_ae_seq_ser = seq_ae_seq.seq.SerializeToString()
     writer_lm.write(lm_seq_ser)
-    writer_rev_lm.write(rev_lm_seq.seq.SerializeToString())
-    writer_seq_ae.write(seq_ae_seq_ser)
+    # writer_rev_lm.write(rev_lm_seq.seq.SerializeToString())
+    # writer_seq_ae.write(seq_ae_seq_ser)
     writer_lm_all.write(lm_seq_ser)
-    writer_seq_ae_all.write(seq_ae_seq_ser)
+    # writer_seq_ae_all.write(seq_ae_seq_ser)
 
   # Close test writers
   writer_lm.close()
-  writer_rev_lm.close()
-  writer_seq_ae.close()
+  # writer_rev_lm.close()
+  # writer_seq_ae.close()
   writer_class.close()
-  writer_bd_class.close()
+  # writer_bd_class.close()
 
 
 def main(_):
